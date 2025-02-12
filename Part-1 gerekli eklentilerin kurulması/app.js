a2ab0e5a95e320417c5cca882ec5b1c738ed801e
Ozyasar <canozyasar35@gmail.com>
2025-02-12 20:40:46 +0300

    Add initial HTML and JavaScript files with basic structure and console logs

---
+++ Part-1 gerekli eklentilerin kurulması/app.js
@@ -0,0 +1,16 @@
+// yeşil renk: comment
+// * açık yeşil renk: comment
+// ? mavi renk: comment
+// ! kırmızı renk: comment
+// todo turuncu renk comment yapılacaklar için kullanılır
+
+
+
+
+// Press Cmd+Shift+L tüm console logları siler eklentimiz sayesinde 
+
+console.log("Merhaba Kodlama.io")
+console.log("Merhaba Kodlama.io1")
+console.log("Merhaba Kodlama.io2")
+console.log("Merhaba Kodlama.io3")
+console.log("Merhaba Kodlama.io4")
