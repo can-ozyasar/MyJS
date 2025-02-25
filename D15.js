dc54b1f09dca09068ec47cfdeda6f5148f57f353
Ozyasar <canozyasar35@gmail.com>
2025-02-25 21:23:18 +0300

    Add JavaScript files for a fuel station application and average calculation

--- D15.js
+++ D15.js
@@ -14,6 +14,13 @@ alert("Merhaba")
 let isim=prompt("Adınızı giriniz")
 console.log(isim)
 
+
+// stringden nunmber tipinde veri almak için
+//Number() fonksiyonu kullanılır
+let yas=Number(prompt("yasınızı giriniz"))
+console.log(yas)
+
+
 //! confirm kullanıcıya onaylatma işlemi yapar 
 // dönen değer true yada false olarak alınabilir, değişkene atanabilir
 let silinsinMi=confirm("Emin misiniz")
@@ -25,3 +32,26 @@ console.log(silinsinMi)
 //? kullanıcıdan alınan değerler hep strin olarak alınır .
 //? isteğe göre dönüştürme yapılabilir
 //? örnek parseInt() fonksiyonu ile sayıya dönüştürme yapılabilir
+
+
+
+//basit ortalama hesaplayan fonksiyon yapımı 
+let sayi1=Number(prompt("1. sayıyı giriniz"))
+let sayi2=Number(prompt("2. sayıyı giriniz"))
+
+
+function ortlamaBul(sayi1,sayi2){
+ let ortalama=( (sayi1+sayi2)/2)
+ if(ortalama<50){
+     alert("Ortalama 50 den küçük : "+ortalama)
+     console.log("+  ortalama : "+ortalama)
+ }
+    else{
+        alert("Ortalama 50 den büyük "+ortalama)
+        console.log("+  ortalama : "+ortalama)
+    }
+ 
+}
+ortlamaBul(sayi1,sayi2)
+
+
---
+++ D16BenzinUYG.js
@@ -0,0 +1,86 @@
+ // benzin istasyonu uygulaması 
+
+ /*
+ 
+ 
+ - benzin istasyonunda 3 çeşit yakıt bulunmaktadır
+    - kullanıcıdan alınan litre miktarına göre ödenecek tutarı hesaplayan programı yazınız
+    - benzin fiyatları şu şekildedir
+ 
+ 
+ 1- benzin : 49 TL
+ 2-DİZEL : 47 TL
+ 3-LPG :25 TL
+ 
+ ALINACAK BİLGİLER
+ 1 YAKIT TİPİ (1,2,3)
+ 2 ALINACAK LİTRE MİKTARI
+ 
+ */
+
+
+ let bakiye=200;
+ var yeterli=false;
+
+
+ function benzinIstasyonu(toplamTutar){
+    if(toplamTutar>bakiye){
+        alert("Yetersiz bakiye")
+    }
+    else{
+        yeterli=true
+        bakiye=bakiye-toplamTutar
+    }
+}
+ 
+
+ do {
+    
+ let dizel=47
+ let benzin=49 
+ let lpg=25
+ const yeniSatır="\r\n"
+ let toplamTutar=0
+ let secilenYakit=""
+
+ let yakitTipi=Number(prompt("Yakıt tipini seçiniz "+yeniSatır+"1-BENZİN"+yeniSatır+"2-DİZEL"+yeniSatır+"3-LPG"+yeniSatır+"4 çıkış"))
+    if(yakitTipi==4){
+    yeterli=false;
+    break;
+    }
+    
+          
+
+    let litre=Number(prompt("Kaç litre alacaksınız"))
+   
+    
+    if(yakitTipi==1){
+        secilenYakit="BENZİN"
+        toplamTutar=benzin*litre 
+    
+        benzinIstasyonu(toplamTutar)
+    
+    }
+       
+    else if(yakitTipi==2){
+        secilenYakit="DİZEL"
+        toplamTutar=dizel*litre
+        benzinIstasyonu(toplamTutar)
+
+    }
+    else if(yakitTipi==3){
+        secilenYakit="LPG"
+        toplamTutar=lpg*litre
+        benzinIstasyonu(toplamTutar)
+
+    }
+    else{
+        alert("Geçersiz yakıt tipi")
+    }
+
+    
+} while (yeterli);
+
+    document.write("Seçilen yakıt tipi : "+secilenYakit+yeniSatır)
+    document.write("Alınan litre miktarı : "+litre+yeniSatır)
+    document.write("Ödenecek tutar : "+toplamTutar+yeniSatır)
